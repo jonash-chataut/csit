ad1e526c7df6b0de9a1645d1ff977eba1fc6bf6a
jonash-chataut <jonaschataut777@gmail.com>
2025-10-14 22:21:56 +0545

    Lab 5 oop

---
+++ second sem/oop/lab code/lab 5/complex multi pass by value.cpp
@@ -0,0 +1,35 @@
+//WAP to multiply two complex numbers using the concept of pass by value taking object as an argument.
+#include<iostream>
+using namespace std;
+class complex
+{
+	int r,i;
+	public:
+		void get()
+		{
+			cout<<"Enter the real and imaginary part of the complex number:"<<endl;
+			cin>>r>>i;
+		}
+		
+		void show()
+		{
+			cout<<r<<"+"<<"i"<<i<<endl;
+		}
+		
+		void multiply(complex x, complex y)
+		{
+			r=x.r*y.r-x.i*y.i;
+			i=x.r*y.i+x.i*y.r;
+		}
+};
+
+int main()
+{
+	cout<<"Using pass by value concept"<<endl;
+	complex a,b,c;
+	a.get();
+	b.get();
+	c.multiply(a,b);
+	c.show();
+	return 0;
+}
